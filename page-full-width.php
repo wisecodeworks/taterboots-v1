8b2ce79e4d6a0e4309923adac33f327da7489094
wisecodeworks <timmtheprogrammer@gmail.com>
2015-12-16 17:52:22 -0500

    These are the main theme files
    These are the main theme files

---
+++ page-full-width.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Template Name: Full Width Page
+*/
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content" class="clearfix row">
+			
+				<div id="main" class="col col-lg-12 clearfix" role="main">
+
+					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					
+					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+						
+						<header>
+
+							<!-- BACKGROUNG IMAGE HERE IF DESIRED -->
+
+		<div class="jumbotron" style="background-image: url(''); background-repeat: no-repeat; background-position: 0 0; height:287px;">
+				
+							
+								
+							</div>
+							
+							<div class="page-header" style="border-bottom: 5px solid #006699;"><h1><?php the_title(); ?></h1></div>
+						
+						</header> <!-- end article header -->
+					
+						<section class="post_content">
+							<?php the_content(); ?>
+					<!-- end article section -->
+						</section> 
+						
+						<footer>
+			
+							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","taterboots") . ': ', ', ', '</span>'); ?></p>
+							
+						</footer> <!-- end article footer -->
+					
+					</article> <!-- end article -->
+					
+					<?php comments_template(); ?>
+					
+					<?php endwhile; ?>	
+					
+					<?php else : ?>
+					
+					<article id="post-not-found">
+					    <header>
+					    	<h1><?php _e("Not Found", "taterboots"); ?></h1>
+					    </header>
+					    <section class="post_content">
+					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "taterboots"); ?></p>
+					    </section>
+					    <footer>
+					    </footer>
+					</article>
+					
+					<?php endif; ?>
+			
+				</div> <!-- end #main -->
+    
+				<?php //get_sidebar(); // sidebar 1 ?>
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
